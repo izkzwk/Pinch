236b968c56e441425b3040c1a0c6cf9be6abdc5b
Dzmitry Bladyka <dzmitrydistortion@gmail.com>
2023-03-27 19:49:13 +0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Dzmitry Bladyka on 27.03.23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
